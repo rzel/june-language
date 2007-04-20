5a47e9e645e72db65dfcecfe6c50e24dc24c4ec6
tjpalmer <tjpalmer@9deba6ae-e02d-0410-af26-4b07b8a663e0>
2007-04-20 05:06:40 +0000

    Investigating resolver needs.

--- src/main/java/june/engine/Resolver.java
+++ src/main/java/june/engine/Resolver.java
@@ -1,7 +1,21 @@
 package june.engine;
 
-public class Resolver {
+public abstract class Resolver {
 
 	// TODO Keep abstract enough that it could apply other than with Java classpaths?
+
+	// TODO How do we represent a signified entity (method, field, property, class, package)? Just saying "Object" for now.
+
+	public Resolver parent;
+
+	protected abstract Object findCurrentEntity(Object signature);
+
+	public Object findEntity(Object signature) {
+		Object entity = findCurrentEntity(signature);
+		if (entity == null) {
+			entity = parent.findEntity(signature);
+		}
+		return entity;
+	}
 
 }
