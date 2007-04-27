aab1135f7b024d064b48fb11ef06a07aff514a6e
tjpalmer <tjpalmer@9deba6ae-e02d-0410-af26-4b07b8a663e0>
2007-04-27 05:45:50 +0000

    A bit more work on resolution. Loading dependent classes and storing in cache of globals.

--- trunk/src/main/java/june/engine/Analyzer.java
+++ trunk/src/main/java/june/engine/Analyzer.java
@@ -1,10 +1,10 @@
 package june.engine;
 
+import static june.tree.TokenType.*;
+
 import java.util.*;
 
 import june.tree.*;
-
-import static june.tree.TokenType.*;
 
 @SuppressWarnings("unchecked")
 public class Analyzer {
@@ -22,6 +22,9 @@ public class Analyzer {
 	// TODO Resolve names. Find higher-level bugs. More.
 
 	// TODO Pass in a Resolver! (Into the constructor?)
+
+	private Map<String, JuneClass> classCache =
+			new HashMap<String, JuneClass>();
 
 	public void analyze(Script script) {
 		for (Node kid: script.getKids()) {
@@ -45,24 +48,35 @@ public class Analyzer {
 				Token token = (Token)kid;
 				if (token.type == ID) {
 					System.out.println("call " + token + " at " + context);
+					// TODO Do we need to know the arg types first?
 					Usage usage = new Usage();
 					usage.name = token.text.toString();
 					if (context instanceof Block) {
 						call.entity =
 								new Resolver.ImportResolver(
 										DEFAULT_IMPORTS,
+										classCache,
 										null).findEntity(usage);
 					} else if (context instanceof Call) {
 						Entity entity = ((Call)context).entity;
 						if (entity instanceof JuneMember) {
+							JuneClass $class =
+									((JuneClass)((JuneMember)entity).type);
+							if (!classCache.containsKey($class.name)) {
+								String[] packageAndClass =
+										Resolver
+												.splitPackageAndClass($class.name);
+								classCache.put($class.name, $class);
+								Resolver.loadClass(
+										classCache,
+										packageAndClass[0],
+										packageAndClass[1]);
+							}
 							// TODO Do we need resolve the other types? Now or immediately when building the class?
-							call.entity =
-									((JuneClass)((JuneMember)entity).type)
-											.getMember(usage);
+							call.entity = $class.getMember(usage);
 						}
 					}
-					// TODO Search through context to find what this is.
-					// TODO Do we need to know the arg types first?
+					System.out.println(call.entity);
 				}
 			}
 		}
--- trunk/src/main/java/june/engine/Resolver.java
+++ trunk/src/main/java/june/engine/Resolver.java
@@ -15,6 +15,8 @@ public abstract class Resolver {
 	 */
 	public static class ImportResolver extends Resolver {
 
+		private final Map<String, JuneClass> classCache;
+
 		/**
 		 * TODO Minimal VFS to support network, in-memory, and so on? Shouldn't use Java 6 APIs for Java 5 compatibility, however.
 		 */
@@ -25,73 +27,46 @@ public abstract class Resolver {
 		/**
 		 * @param imports
 		 *            (or should this be a list?) - can't really accommodate current package, since that has a specific priority. And what about "import bob: java.sql"?
+		 * @param classCache
 		 * @param classpath
 		 */
-		public ImportResolver(Set<String> imports, List<String> classpath) {
+		public ImportResolver(
+				Set<String> imports,
+				Map<String, JuneClass> classCache,
+				List<String> classpath) {
 			this.imports = imports;
+			this.classCache = classCache;
 			this.classpath = classpath;
 		}
 
-		private void addMatches(
-				Set<Entity> matches,
-				String $import,
-				Usage signature) {
+		private void addMatches(Set<Entity> matches, String $import, Usage usage) {
 			try {
 				// TODO First check the last part of the imported package to see if it matches (if null argTypes). As in "sql" for "java.sql".
 				// TODO Use the provided classpath, and manually search it instead of using Package#getPackage(String).
-				String packageName = $import;
-				Package $package = Package.getPackage(packageName);
-				String baseClassName = null;
-				while (packageName.length() > 0 && $package == null) {
-					int lastDot = packageName.lastIndexOf('.');
-					baseClassName =
-							packageName.substring(lastDot + 1)
-									+ (baseClassName == null ? "" : "."
-											+ baseClassName);
-					packageName =
-							packageName.substring(0, lastDot < 0 ? 0 : lastDot);
-					$package = Package.getPackage(packageName);
-					// TODO Cache some of these search results?
-				}
+				String[] packageAndClass = splitPackageAndClass($import);
+				String packageName = packageAndClass[0];
+				String baseClassName = packageAndClass[1];
 				boolean expectClass = false;
 				if (baseClassName == null) {
 					// There was no class in the import, so the requested entity must be a class or package.
 					String tempPackageName =
 							packageName + (packageName.length() > 0 ? "." : "")
-									+ signature.name;
+									+ usage.name;
 					if (Package.getPackage(tempPackageName) != null) {
 						matches.add(new JunePackage());
 						return;
 					}
-					baseClassName = signature.name;
+					baseClassName = usage.name;
 					expectClass = true;
 				}
-				String resourceName =
-						(packageName.length() > 0 ? packageName.replace(
-								'.',
-								'/')
-								+ "/" : "")
-								+ baseClassName.replace('.', '$') + ".class";
-				System.out.println(resourceName);
-				// TODO Just search the classpath manually where possible, but this might still be needed for system classes.
-				URL url = getClass().getClassLoader().getResource(resourceName);
-				if (url != null) {
-					// TODO Cache built classes by URL or by resourceName?
-					System.out.println(url);
-					ClassBuilder builder = new ClassBuilder();
-					InputStream stream = url.openStream();
-					try {
-						new ClassReader(new BufferedInputStream(stream))
-								.accept(builder, ClassReader.SKIP_CODE);
-					} finally {
-						stream.close();
-					}
-					JuneClass $class = builder.$class;
+				JuneClass $class =
+						loadClass(classCache, packageName, baseClassName);
+				if ($class != null) {
 					System.out.println($class);
 					if (expectClass) {
 						matches.add($class);
 					} else {
-						JuneMember member = $class.getMember(signature);
+						JuneMember member = $class.getMember(usage);
 						// TODO Watch for visibility and static only.
 						if (member != null) {
 							matches.add(member);
@@ -138,6 +113,58 @@ public abstract class Resolver {
 
 	}
 
+	public static JuneClass loadClass(
+			Map<String, JuneClass> classCache,
+			String packageName,
+			String baseClassName) {
+		try {
+			JuneClass $class = null;
+			String resourceName =
+					(packageName.length() > 0 ? packageName.replace('.', '/')
+							+ "/" : "")
+							+ baseClassName.replace('.', '$') + ".class";
+			String className =
+					(packageName.length() > 0 ? (packageName + '.') : "")
+							+ baseClassName;
+			// TODO Just search the classpath manually where possible, but this might still be needed for system classes.
+			URL url = Resolver.class.getClassLoader().getResource(resourceName);
+			if (url != null) {
+				// TODO Cache built classes by URL or by resourceName?
+				System.out.println(url);
+				ClassBuilder builder =
+						new ClassBuilder(classCache.get(className));
+				InputStream stream = url.openStream();
+				try {
+					new ClassReader(new BufferedInputStream(stream)).accept(
+							builder,
+							ClassReader.SKIP_CODE);
+				} finally {
+					stream.close();
+				}
+				$class = builder.$class;
+			}
+			return $class;
+		} catch (Exception e) {
+			throw Helper.throwAny(e);
+		}
+	}
+
+	public static String[] splitPackageAndClass(String qualifiedName) {
+		String packageName = qualifiedName;
+		String baseClassName = null;
+		Package $package = Package.getPackage(packageName);
+		while (packageName.length() > 0 && $package == null) {
+			int lastDot = packageName.lastIndexOf('.');
+			baseClassName =
+					packageName.substring(lastDot + 1)
+							+ (baseClassName == null ? "" : "." + baseClassName);
+			packageName = packageName.substring(0, lastDot < 0 ? 0 : lastDot);
+			$package = Package.getPackage(packageName);
+			// TODO Cache some of these search results?
+		}
+		return new String[] {packageName, baseClassName};
+	}
+
 	// TODO Keep abstract enough that it could apply other than with Java classpaths?
 
 	// TODO How do we represent a signified entity (method, field, property, class, package)? Just saying "Object" for now.
@@ -151,7 +178,6 @@ public abstract class Resolver {
 		if (entity == null && parent != null) {
 			entity = parent.findEntity(signature);
 		}
-		System.out.println(entity);
 		return entity;
 	}
 
