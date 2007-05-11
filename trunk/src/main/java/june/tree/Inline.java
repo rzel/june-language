ba728fa4b62374ac6729acc3469b8be6d64cb1f6
tjpalmer <tjpalmer@9deba6ae-e02d-0410-af26-4b07b8a663e0>
2007-05-11 04:37:19 +0000

    Some cleanup (leftovers from Tuga) and a bit of looking into vars.

--- trunk/src/main/java/june/tree/Inline.java
+++
@@ -1,8 +0,0 @@
-package june.tree;
-
-/**
- * Represents inline (in statement) nodes rather than full statements (with indent/comment) or blocks.
- */
-public abstract class Inline extends Node {
-
-}
--- trunk/src/main/java/june/tree/Params.java
+++ trunk/src/main/java/june/tree/Params.java
@@ -1,11 +1,5 @@
 package june.tree;
 
-public class Params extends Inline {
-
-	@Override
-	public Iterable<Node> getKids() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+public class Params extends Parent {
 
 }
