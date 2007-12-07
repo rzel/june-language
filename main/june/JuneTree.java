d2fc0326ab5f95b9fd483a05e8d64a6083e0e5c2
tjpalmer <tjpalmer@9deba6ae-e02d-0410-af26-4b07b8a663e0>
2007-12-07 06:46:53 +0000

    Prepare for work on Analyzer finding types and symbols.

---
+++ main/june/JuneTree.java
@@ -0,0 +1,14 @@
+package june;
+
+import org.antlr.runtime.*;
+import org.antlr.runtime.tree.*;
+
+public class JuneTree extends CommonTree {
+
+	public Class<?> type;
+
+	public JuneTree(Token payload) {
+		super(payload);
+	}
+
+}
