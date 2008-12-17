2069be43e463ff6da8bfa06ae581ba2405cbc62c
tjpalmer <tjpalmer@9deba6ae-e02d-0410-af26-4b07b8a663e0>
2008-12-17 19:24:40 +0000

    Renaming Fathom to Rio, and making some progress.

---
+++ trunk/fathom/main/fathom/RioTree.java
@@ -0,0 +1,24 @@
+package fathom;
+
+import java.util.*;
+
+import org.antlr.runtime.*;
+import org.antlr.runtime.tree.*;
+
+public class RioTree extends CommonTree {
+
+	/**
+	 * The enclosing block of code that defines the scope for this node.
+	 */
+	public RioTree block;
+
+	/**
+	 * If this is a block, then non-null, a map from IDs to nodes.
+	 */
+	public Map<String, Set<RioTree>> symbols;
+
+	public RioTree(Token payload) {
+		super(payload);
+	}
+
+}
